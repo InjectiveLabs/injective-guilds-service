c21320f508653f474a03dc05ebca840e32e539ce
vinhphuctadang <vinhphuctadang@gmail.com>
2022-06-28 10:39:36 +0700

    added: params in API responses

--- internal/service/guilds-api/guilds_api.go
+++ internal/service/guilds-api/guilds_api.go
@@ -210,6 +210,7 @@ func (s *service) GetGuildMembers(ctx context.Context, payload *svc.GetGuildMemb
 			InjectiveAddress:     m.InjectiveAddress.String(),
 			IsDefaultGuildMember: m.IsDefaultGuildMember,
 			Since:                m.Since.UnixMilli(),
+			Params:               m.Params,
 		})
 	}
 
@@ -269,6 +270,7 @@ func (s *service) GetGuildDefaultMember(ctx context.Context, payload *svc.GetGui
 		DefaultMember: &svc.GuildMember{
 			InjectiveAddress:     defaultMember[0].InjectiveAddress.String(),
 			IsDefaultGuildMember: defaultMember[0].IsDefaultGuildMember,
+			Params:               defaultMember[0].Params,
 		},
 	}, nil
 }
@@ -754,6 +756,7 @@ func (s *service) GetAccountInfo(ctx context.Context, payload *svc.GetAccountInf
 			InjectiveAddress:     members[0].InjectiveAddress.String(),
 			IsDefaultGuildMember: members[0].IsDefaultGuildMember,
 			Since:                members[0].Since.UnixMilli(),
+			Params:               members[0].Params,
 		},
 	}, nil
 }
